73e626e84fe18f4edda2d06f46f5bec5e356df2f
MaybeGitHub <jesus.salvador.barcenilla@gmail.com>
2015-10-23 02:51:37 +0200

    busqueda libros funcionando 100%

---
+++ Ajax Visual Studio/AJAX/servidor.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace AJAX
+{
+    public partial class servidor : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string patron = Request.Params["patron"].ToLower();
+            int valor = int.Parse(Request.Params["valor"]);
+
+            XmlDocument miDoc = new XmlDocument();
+            miDoc.Load(Server.MapPath("libreria.xml"));
+
+            List<XmlNode> listaLibros = miDoc.GetElementsByTagName("Libro").Cast<XmlNode>().ToList();
+            List<XmlNode> cribaLibros = listaLibros.Where(libro => libro.ChildNodes[valor].ChildNodes[0].Value.ToLower().StartsWith(patron)).ToList();
+
+            string salida = "<Libreria>";
+            cribaLibros.ForEach(nodo => salida += nodo.OuterXml);
+            salida += "</Libreria>";
+            Response.ContentType = "text/Xml";
+            Response.Write(salida);
+            Response.Flush();
+            Response.End();
+        }
+    }
+}
