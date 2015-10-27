7e84a7567912000d02f8b203cfccdb4350469926
MaybeGitHub <jesus.salvador.barcenilla@gmail.com>
2015-10-27 14:41:04 +0100

    Subidos los archivos para manejar usuarios con JSON. No registra, solo loguea.

---
+++ Ajax Visual Studio/AJAX/Autentificador.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Web.Script.Serialization;
+
+namespace AJAX
+{
+    public class Autentificador
+    {
+        private string path = @"C:/Users/Maybe/Documents/GitHubVisualStudio/Ajax-VisualStudio/Ajax Visual Studio/AJAX/usuarios.txt";
+        private string login;
+        private string password;
+        public string Login
+        {
+            get { return login; }
+            set
+            {
+                login = File.ReadAllLines(path).Where(linea => linea.Split(':')[0] == value).Select(linea => linea.Split(':')[0]).SingleOrDefault();
+                if ( login == null)
+                {
+                    salida += 2;
+                }             
+            }
+        }
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = File.ReadAllLines(path).Where(linea => linea.Split(':')[1] == value).Select(linea => linea.Split(':')[1]).SingleOrDefault();
+                if( login == null || password == null )
+                {
+                    salida += 1;
+                }
+            }
+        }
+        public int salida { get; set; }
+
+        private string[] mensajes = new string[] { "Ok", "Password incorrecto", null, "No existe usuario" };
+
+        public Autentificador()
+        {
+            salida = 0;
+        }
+
+        public override string ToString()
+        {            
+            return "{\"salida\":" + salida + ", \"mensaje\":\"" + mensajes[salida] + "\"}";
+        }
+    }
+}
---
+++ Ajax Visual Studio/AJAX/autentificacion.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Script.Serialization;
+
+namespace AJAX
+{
+    public partial class autentificacion : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Autentificador recibido = new JavaScriptSerializer().Deserialize<Autentificador>(Request.Params["datos"]);
+            Response.Write(recibido.ToString());
+            Response.ContentType = "application/json";
+            Response.End();            
+        }
+    }
+}
