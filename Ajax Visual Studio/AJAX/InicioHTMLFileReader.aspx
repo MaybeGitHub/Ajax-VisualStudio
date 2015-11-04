0a81361a5b721abbb1db795ceca30d93861374d3
MaybeGitHub <jesus.salvador.barcenilla@gmail.com>
2015-11-04 21:38:23 +0100

    Upload ficheros a JSON

---
+++ Ajax Visual Studio/AJAX/InicioHTMLFileReader.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="InicioHTMLFileReader.aspx.cs" Inherits="AJAX.InicioHTMLFileReader" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>FileReader</title>
+</head>
+<body>
+    <input type="file" accept="image/*" id="selector" />
+    <div id="respuestaServer"></div>
+    <script>
+        var petAjax = new XMLHttpRequest();
+        function hacerPeticionAjax() {
+            var fichero = document.getElementById("selector").files[0];
+            var lector = new FileReader(fichero);
+            var datos = new FormData();
+            datos.append("selector", fichero, fichero.name);
+            petAjax.onreadystatechange = function () {
+                if (this.readystate == 4 && this.status == 200) {
+                    var respuesta = JSON.parse(this.responseText);
+                    if (respuesta.codigo == 0) {
+                        document.getElementById("respuestaServer").appendChild(document.createTextNode(respuesta.mensaje));
+                    }
+                }
+            }
+            petAjax.open("post", "http://localhost:2068/servidor.aspx", true);
+            petAjax.send(datos);
+        }
+        document.getElementById("selector").addEventListener("change", hacerPeticionAjax, false);
+
+    </script>
+</body>
+</html>
--- Ajax Visual Studio/AJAX/servidor.aspx.cs
+++ Ajax Visual Studio/AJAX/servidor.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Xml;
+using System.IO;
 
 namespace AJAX
 {
@@ -12,22 +13,43 @@ namespace AJAX
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string patron = Request.Params["patron"].ToLower();
-            int valor = int.Parse(Request.Params["valor"]);
+            if (Request.Params["patron"] != null)
+            {
+                string patron = Request.Params["patron"].ToLower();
+                int valor = int.Parse(Request.Params["valor"]);
 
-            XmlDocument miDoc = new XmlDocument();
-            miDoc.Load(Server.MapPath("libreria.xml"));
+                XmlDocument miDoc = new XmlDocument();
+                miDoc.Load(Server.MapPath("libreria.xml"));
 
-            List<XmlNode> listaLibros = miDoc.GetElementsByTagName("Libro").Cast<XmlNode>().ToList();
-            List<XmlNode> cribaLibros = listaLibros.Where(libro => libro.ChildNodes[valor].ChildNodes[0].Value.ToLower().StartsWith(patron)).ToList();
+                List<XmlNode> listaLibros = miDoc.GetElementsByTagName("Libro").Cast<XmlNode>().ToList();
+                List<XmlNode> cribaLibros = listaLibros.Where(libro => libro.ChildNodes[valor].ChildNodes[0].Value.ToLower().StartsWith(patron)).ToList();
 
-            string salida = "<Libreria>";
-            cribaLibros.ForEach(nodo => salida += nodo.OuterXml);
-            salida += "</Libreria>";
-            Response.ContentType = "text/Xml";
-            Response.Write(salida);
-            Response.Flush();
-            Response.End();
+                string salida = "<Libreria>";
+                cribaLibros.ForEach(nodo => salida += nodo.OuterXml);
+                salida += "</Libreria>";
+                Response.ContentType = "text/Xml";
+                Response.Write(salida);
+                Response.Flush();
+                Response.End();
+            }
+
+            if ( Request.Files.Count != 0 )
+            {                
+                string objetoJSON;
+                try {
+                    HttpPostedFile fichero = Request.Files[0];
+                    byte[] contenido = new byte[fichero.ContentLength];
+                    fichero.InputStream.Read(contenido, 0, fichero.ContentLength);
+                    File.WriteAllBytes("~/AppData/uploads/" + fichero.FileName, contenido);
+                    objetoJSON = "{ 'codigo':0, 'mensaje':'Ha funcionado la subida del fichero'}";
+                } catch
+                {
+                    objetoJSON = "{ 'codigo':1, 'mensaje':'Ha fallado la subida del fichero'}";
+                }
+                Response.Write(objetoJSON.ToString());
+                Response.ContentType = "application/json";
+                Response.End();
+            }
         }
     }
 }
